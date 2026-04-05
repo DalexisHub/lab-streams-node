03346076f7f63cd146ec94a6ae2140a51e2bafc3
DalexisHub <alexisdiego478@gmail.com>
2026-04-04 19:42:57 -0500

    tarea 1 realizada

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
+const { Transform } = require('stream');
 const fs = require('fs');
 
-const readable = fs.createReadStream('datos.txt', { encoding: 'utf8' });
-
-readable.on('data', chunk => console.log('Fragmento recibido:', chunk));
-readable.on('end', () => console.log('Lectura completa'));
-readable.on('error', err => console.error('Error:', err));
-
-const writable = fs.createWriteStream('salida.txt');
-writable.write('Este es un mensaje de prueba.\n');
-writable.end('Fin del mensaje.');
-writable.on('finish', () => console.log('Escritura completada.'));
-
-const zlib = require('zlib');
-const readStream = fs.createReadStream('entrada.txt');
-const writeStream = fs.createWriteStream('entrada.txt.gz');
-const gzip = zlib.createGzip();
-readStream.pipe(gzip).pipe(writeStream);
-
-const writable2 = fs.createWriteStream('salida2.txt');
-
-readable.on('data', chunk => {
-  if (!writable2.write(chunk)) {
-    readable.pause();
+const transformStream = new Transform({
+  transform(chunk, encoding, callback) {
+    callback(null, chunk.toString().toUpperCase());
   }
 });
 
-writable2.on('drain', () => readable.resume());
+const readStream2 = fs.createReadStream('texto.txt');
+const writeStream2 = fs.createWriteStream('texto_mayusculas.txt');
+
+readStream2.pipe(transformStream).pipe(writeStream2);
