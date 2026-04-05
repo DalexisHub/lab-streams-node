b76e0c8b481de99b7c657e79de8da774b2c3cf16
DalexisHub <alexisdiego478@gmail.com>
2026-04-04 19:05:09 -0500

    Segunda  actividad realizada

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const readable = fs.createReadStream('datos.txt', { encoding: 'utf8' });
 readable.on('data', chunk => console.log('Fragmento recibido:', chunk));
 readable.on('end', () => console.log('Lectura completa'));
 readable.on('error', err => console.error('Error:', err));
+
+const writable = fs.createWriteStream('salida.txt');
+writable.write('Este es un mensaje de prueba.\n');
+writable.end('Fin del mensaje.');
+writable.on('finish', () => console.log('Escritura completada.'));
